c4df13308782691bef29aac7182df173d2b693e3
SamuelSempere <chempe@gmail.com>
2025-04-23 19:53:33 +0200

    fix layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 // pages/_app.js
 import { SessionProvider } from "next-auth/react";
-import Layout from '../components/Layout';
+import Layout from '../components/layout';
 import '../styles/globals.css';
 
 export default function MyApp({ Component, pageProps: { session, ...pageProps } }) {
